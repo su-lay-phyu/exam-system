2916fb61eff17affbacd577894c3a0ad06c12a05
SuLayPhyu02 <sulayphyu6619@gmail.com>
2023-12-01 08:50:36 +0630

    last time

--- src/main/java/com/nexcode/examsystem/config/WebMvcConfig.java
+++ src/main/java/com/nexcode/examsystem/config/WebMvcConfig.java
@@ -14,7 +14,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-	    .allowedOrigins("http://localhost:5173","http://localhost:5174") 
+	    .allowedOrigins("http://localhost:5173","http://localhost:5174","http://localhost:5175","http://localhost:5176") 
 	    .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
 	    .maxAge(MAX_AGE_SECS)
 	    .allowedHeaders("*")
